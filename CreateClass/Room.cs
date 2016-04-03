16f3aa8b5253975fe9a8cde1321220e6418d4976
zavox <andras.kesik@gmail.com>
2016-04-03 18:23:51 +0200

    done

---
+++ CreateClass/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Room
+    {
+        private int number;
+        public int Number { get; set; }
+
+        public Room(int number)
+        {
+            this.number = number;
+        }
+    }
+}
---
+++ TimerEvents/Form.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerEvents
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        Timer t;
+        public Form()
+        {
+            InitializeComponent();  
+        }
+       
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            t = new Timer();
+            t.Interval = 1000;
+            t.Tick += t_Tick;
+
+            t.Start();
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            progressBar.Value += 10;
+            if (progressBar.Value >= 100) t.Stop();
+        }
+    }
+}
